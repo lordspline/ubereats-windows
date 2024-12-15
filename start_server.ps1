2ffe90e1b70de1a79bfc2f578eaaceddb12eea78
nalin <74811063+lordspline@users.noreply.github.com>
2024-12-15 14:22:58 -0800

    Update start_server.ps1

--- start_server.ps1
+++ start_server.ps1
@@ -3,5 +3,7 @@ $env:PYTHONPATH = "."
 $env:WIDTH = "1920"
 $env:HEIGHT = "1080"
 
+New-NetFirewallRule -DisplayName "Allow FastAPI 8000" -Direction Inbound -LocalPort 8000 -Protocol TCP -Action Allow
+
 # Start the FastAPI server
 python -m uvicorn computer.main:app --host 0.0.0.0 --port 8000
